87d8c56c4bef7269136ef0622e0c5e04cc5553a9
rkeerthient <rkeerthi@yext.com>
2023-08-12 15:38:13 +0530

    updated

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -63,7 +63,7 @@ export const getHeadConfig: GetHeadConfig<
   TemplateRenderProps
 > = (): HeadConfig => {
   return {
-    title: "Static Page Example",
+    title: "Outreach | Home",
     charset: "UTF-8",
     viewport: "width=device-width, initial-scale=1",
     tags: [
